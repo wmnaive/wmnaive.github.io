13f37a133c6b708659d3362a07a268741d711246
wmnaive <169973282@qq.com>
2023-08-29 01:18:25 +0800

    Site updated: 2023-08-29 01:18:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/28/wm/","2023/08/28/adobe/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/28/wm/","2023/08/28/adobe/","2023/08/29/aecs6/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
